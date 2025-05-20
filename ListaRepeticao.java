c612d5d45589508f45d0ff31a27d73a830968894
Isaque Gabriel <isaquegabriel8@hotmail.com>
2025-05-20 12:05:57 -0300

    Delete ListaRepeticao.java

--- ListaRepeticao.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-
-public class Lista {
-    public static void main(String[] args) throws Exception {
-        // Scanner entrada=new Scanner(System.in);
-        System.out.println("Ola");
-    }
-}
